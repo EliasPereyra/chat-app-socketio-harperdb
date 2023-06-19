b622c270fcd132af0d76ad6122d38d92a26c6dfb
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-06-18 23:33:30 -0300

    Add a new get route to the server

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,29 @@
 import express from 'express'
+import http from 'http'
 import cors from 'cors'
+import { Server } from 'socket.io'
 
 const PORT = 4000
 const app = express()
+app.use(cors())
+const server = http.createServer(app)
 
 app.use(express.json())
-app.use(cors())
+
+const socketServer = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173/',
+    methods: ['GET', 'POST']
+  }
+})
+
+socketServer.on('connection', (socket) => {
+  console.log(`User connected ${socket.id}`)
+})
+
+app.get("/", (_, res) => {
+  res.send("Hello there, this is the server")
+})
 
 app.get("/checkhealth", (_, res) => {
   res.send("Everything is working")
