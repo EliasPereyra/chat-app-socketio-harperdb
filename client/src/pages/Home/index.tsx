2c880275c54e7ca2899441b8e5a6ed139056dfa6
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-06-19 23:27:50 -0300

    Use the props that are passed to this component

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,13 +1,44 @@
+import React from "react";
+import { Socket, io } from "socket.io-client";
+import { useNavigate } from "react-router-dom";
+
 import "./styles.css";
 
-export default function Home() {
+export default function Home({
+  username,
+  setUsername,
+  room,
+  setRoom,
+  socket,
+}: {
+  username: string;
+  setUsername: React.Dispatch<React.SetStateAction<string>>;
+  room: string;
+  setRoom: React.Dispatch<React.SetStateAction<string>>;
+  socket: Socket;
+}) {
+  const navigate = useNavigate();
+  const joinRoom = () => {
+    if (room !== "" && username !== "") {
+      socket.emit("join_room", { username, room });
+    }
+
+    navigate("/chat", { replace: true });
+  };
+
   return (
     <section className="container">
       <h1 className="home-title">Home</h1>
 
       <div className="form">
-        <input className="input-home" type="text" placeholder="Username" />
-        <select className="input-home">
+        <input
+          value={username}
+          className="input-home"
+          type="text"
+          placeholder="Username"
+          onChange={e => setUsername(e.target.value)}
+        />
+        <select className="input-home" onChange={e => setRoom(e.target.value)}>
           <option>Select a room</option>
           <option value="php">PHP</option>
           <option value="java">Java</option>
@@ -16,7 +47,7 @@ export default function Home() {
           <option value="go">Go</option>
           <option value="rust">Rust</option>
         </select>
-        <button>Join room</button>
+        <button onClick={joinRoom}>Join room</button>
       </div>
     </section>
   );
