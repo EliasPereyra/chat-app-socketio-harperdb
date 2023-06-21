3cb8b1e0a8cddd56a1b5363e07e342703eed1db0
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-06-20 23:29:49 -0300

    Pass the props to the Home component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,34 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Socket, io } from "socket.io-client";
+
 import Home from "./pages/Home";
+
 import "./App.css";
+import NotFound from "./pages/NotFound";
+
+const socketInstance: Socket = io("http://localhost:4000");
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              username={username}
+              setUsername={setUsername}
+              room={room}
+              setRoom={setRoom}
+              socket={socketInstance}
+            />
+          }
+        />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
