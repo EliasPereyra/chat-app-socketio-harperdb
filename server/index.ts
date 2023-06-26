65b699a31e6a07202d33c93b92007930650a3f6c
Elias Pereyra Gomez <EliasPereyra_Gomez@hotmail.com>
2023-06-25 22:00:14 -0300

    Feat: retrieve all users who connect to the room

--- server/index.ts
+++ server/index.ts
@@ -1,34 +1,46 @@
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
+import https from 'node:https'
 import { Server } from 'socket.io'
 
 const PORT = 4000
-const app = express()
-app.use(cors())
-const server = http.createServer(app)
+const server = https.createServer()
 
-app.use(express.json())
+const CHAT_BOT = Symbol('ChatBot')
+let chatRoomUsers = []
+let allUsers = []
 
 const socketServer = new Server(server, {
   cors: {
-    origin: 'http://localhost:5173/',
+    origin: 'http://localhost:5173',
     methods: ['GET', 'POST']
   }
 })
 
 socketServer.on('connection', (socket) => {
   console.log(`User connected ${socket.id}`)
+
+  socket.on("join_room", (data) => {
+    const { username, room } = data
+    socket.join(room)
+
+    let __createdTime__ = Date.now()
+    socket.to(room).emit('receive_message', {
+      message: `${username} has joined the chat room`,
+      username: CHAT_BOT,
+      __createdTime__
+    })
+
+    socket.emit('receive_message', {
+      message: `Welcome ${username}`,
+      username: CHAT_BOT,
+      __createdTime__
+    })
+
+    let chatRoomUsers
+    allUsers.push({ id: socket.id, username, room })
+    chatRoomUsers = allUsers.filter(user => user.room === room)
+    socket.to(room).emit('chatroom_users', chatRoomUsers)
+    socket.emit('chatroom_users', chatRoomUsers)
+  })
 })
 
-app.get("/", (_, res) => {
-  res.send("Hello there, this is the server")
-})
-
-app.get("/checkhealth", (_, res) => {
-  res.send("Everything is working")
-})
-
-app.listen(PORT, () => {
-  console.info(`Server running on port ${PORT}`)
-})
+socketServer.listen(PORT)
